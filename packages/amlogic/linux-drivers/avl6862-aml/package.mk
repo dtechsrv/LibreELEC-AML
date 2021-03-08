b5f7cd50749ca9e049044c90e4cc063ddf9f4611
dtechsrv <dee.gabor@gmail.com>
2021-03-08 19:09:20 +0100

    move avl6862 driver to an external kernel module, add support for MXQ 4K Pro+

--- packages/amlogic/linux-drivers/avl6862-aml/package.mk
+++ packages/amlogic/linux-drivers/avl6862-aml/package.mk
@@ -7,9 +7,23 @@ PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="virtual"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_LONGDESC="avl6862-aml: Internal DVB tuner driver for Amlogic devices developed by afl1"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  kernel_make -C "$(kernel_path)" M="$PKG_BUILD"
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
+}
 
 post_install() {
   enable_service amlogic-dvb.service
