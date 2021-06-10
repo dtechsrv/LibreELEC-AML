f4f43a5a56928833487d9270c4cca58a630dc247
dtechsrv <dee.gabor@gmail.com>
2021-06-10 20:08:11 +0200

    build script: update supported and add a new one for 3rdParty devices

--- build.sh
+++ build.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
+# Create image for supported devices
 
-# Create image for Mecool/Videostrong KI Pro (gxl_p230_k1_pro)
-# Specs: Amlogic S905D SoC, 2 GB MB RAM, 16 GB NAND Flash, 1 Gbps Ethernet (RTL8211F),
+# Mecool/Videostrong KI Pro (gxl_p230_k1_pro)
+# Specs: Amlogic S905D SoC, 2 GB RAM, 16 GB NAND Flash, 1 Gbps Ethernet (RTL8211F),
 # Tested and works on PCB: 'K3_DVB_DDR4-V1.2', wireless module: ap6255.
 PROJECT=S905 DEVICE=KI-Pro ARCH=arm make image -j8
 
-# Create image for MXQ 4K Pro+ (gxl_p212_2g_nand)
-# Specs: Amlogic S905X SoC, 2 GB MB RAM, 16 GB NAND Flash, 10/100 Mbps Ethernet (IC+ IP101GR),
+# MXQ 4K Pro+ (gxl_p212_2g_nand)
+# Specs: Amlogic S905X SoC, 2 GB RAM, 16 GB NAND Flash, 10/100 Mbps Ethernet (IC+ IP101GR),
 # Tested and works with wireless module: bcm4334b1.
 PROJECT=S905 DEVICE=MXQ-4K-Pro_2G ARCH=arm make image -j8
 
-# Create image for S8X2/M8S+ (m8m2_KII)
+# M8S+ (m8m2_KII)
 # Specs: Amlogic S812 SoC, 2 GB RAM, 8 GB NAND Flash, 1 Gbps Ethernet (RTL8211F),
 # Tested and works on PCB: 'S812_M8_REV1.1 15/05/21', wireless module: ap6330 or bcm4335b0.
 PROJECT=S8X2 DEVICE=M8S-Plus ARCH=arm make image -j8
 
-# Create image for MXQ-HD18Q "all black" boxes (m8b_m201_v1)
+# MXQ-HD18Q "all black" boxes (m8b_m201_v1)
 # Specs: Amlogic S805 SoC, 1 GB RAM, 8 GB NAND Flash, 10/100 Mbps Ethernet (IC+ IP101GR),
 # Tested and works on PCB: M8S-S805, M8Q labeled board in an M8 case.
 PROJECT=S805 DEVICE=HD18Q ARCH=arm make image -j8
 
-# Create image for MXQ-m201d with 512 MB RAM only (m8b_m201C_512M)
+# MXQ-m201d with 512 MB RAM only (m8b_m201C_512M)
 # Specs: Amlogic S805 SoC, 1 GB RAM, 8 GB NAND Flash, 10/100 Mbps Ethernet (IC+ IP101GR),
 # Tested and works on PCB: 'S805 Rev2.0 20150806'.
 PROJECT=S805 DEVICE=m201d ARCH=arm make image -j8
+
+# WeTek_Play (AML8726-MX, meson6 without dt-id)
+# Specs: Amlogic M6 SoC, 1 GB RAM, 4 GB NAND Flash, 10/100 Mbps Ethernet (SMSC LAN8720A),
+# Tested and works on PCB: 'MX 05/16'.
+PROJECT=WeTek_Play ARCH=arm make image -j8
 
 # Remove unnecessary files after the build process
 rm -rf target/*.kernel
---
+++ build_3rdParty.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Create image for 3rdParty devices
+
+# MXIII
+# Specs: Amlogic S802 SoC, 1 GB RAM, 10/100 Mbps LAN, AP6330 WiFi
+PROJECT=Minix-S8X2 DEVICE=MXIII-1G ARCH=arm make image -j8
+
+# MXIII Plus
+# Specs: Amlogic S812 SoC, 2 GB RAM, Gigabit LAN, AP6234 WiFi
+PROJECT=Minix-S8X2 DEVICE=MXIII-Plus ARCH=arm make image -j8
+
+# MINIX Neo X8-H
+# Specs: Amlogic S812-H SoC, 2 GB RAM, 10/100 Mbps LAN, AP6335e WiFi
+PROJECT=Minix-S8X2 DEVICE=X8H-Plus ARCH=arm make image -j8
+
+# WeTek Core
+# Specs: Amlogic S812-H SoC, 2 GB RAM, Gigabit LAN, AP6330 WiFi
+PROJECT=WeTek_Core ARCH=arm make image -j8
+
+# MXQ-S85
+# Specs: Amlogic S805 SoC, 1 GB RAM, 10/100 LAN, AP6210/AP6212 WiFi
+PROJECT=S805 DEVICE=MXQ ARCH=arm make image -j8
+
+# Remove unnecessary files after the build process
+rm -rf target/*.kernel
+rm -rf target/*.system
