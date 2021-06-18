23e34f99979573c52125fb55b675ac61eae2f9f5
dtechsrv <dee.gabor@gmail.com>
2021-06-18 16:04:13 +0200

    fix device names and descriptions in build scripts

--- build.sh
+++ build.sh
@@ -11,24 +11,24 @@ PROJECT=S905 DEVICE=KI-Pro ARCH=arm make image -j8
 # Tested and works with wireless module: bcm4334b1.
 PROJECT=S905 DEVICE=MXQ-4K-Pro_2G ARCH=arm make image -j8
 
-# M8S+ (m8m2_KII)
+# OTT M8S+ (m8m2_KII)
 # Specs: Amlogic S812 SoC, 2 GB RAM, 8 GB NAND Flash, 1 Gbps Ethernet (RTL8211F),
 # Tested and works on PCB: 'S812_M8_REV1.1 15/05/21', wireless module: ap6330 or bcm4335b0.
 PROJECT=S8X2 DEVICE=M8S-Plus ARCH=arm make image -j8
 
-# MXQ-HD18Q "all black" boxes (m8b_m201_v1)
+# MXQ HD18Q "all black" boxes (m8b_m201_v1)
 # Specs: Amlogic S805 SoC, 1 GB RAM, 8 GB NAND Flash, 10/100 Mbps Ethernet (IC+ IP101GR),
 # Tested and works on PCB: M8S-S805, M8Q labeled board in an M8 case.
 PROJECT=S805 DEVICE=HD18Q ARCH=arm make image -j8
 
-# MXQ-m201d with 512 MB RAM only (m8b_m201C_512M)
+# MXQ m201d with 512 MB RAM only (m8b_m201C_512M)
 # Specs: Amlogic S805 SoC, 1 GB RAM, 8 GB NAND Flash, 10/100 Mbps Ethernet (IC+ IP101GR),
 # Tested and works on PCB: 'S805 Rev2.0 20150806'.
 PROJECT=S805 DEVICE=m201d ARCH=arm make image -j8
 
-# WeTek_Play (AML8726-MX, meson6 without dt-id)
+# WeTek Play (AML8726-MX, meson6 without dt-id)
 # Specs: Amlogic M6 SoC, 1 GB RAM, 4 GB NAND Flash, 10/100 Mbps Ethernet (SMSC LAN8720A),
-# Tested and works on PCB: 'MX 05/16'.
+# Tested and works on WeTek Play OpenELEC Edition box.
 PROJECT=WeTek_Play ARCH=arm make image -j8
 
 # Remove unnecessary files after the build process
--- build_3rdParty.sh
+++ build_3rdParty.sh
@@ -5,23 +5,23 @@
 # Specs: Amlogic S802 SoC, 1 GB RAM, 10/100 Mbps LAN, AP6330 WiFi
 PROJECT=Minix-S8X2 DEVICE=MXIII-1G ARCH=arm make image -j8
 
-# MXIII Plus
+# MXIII Plus / G
 # Specs: Amlogic S812 SoC, 2 GB RAM, Gigabit LAN, AP6234 WiFi
 PROJECT=Minix-S8X2 DEVICE=MXIII-Plus ARCH=arm make image -j8
 
-# MINIX Neo X8-H
+# MINIX Neo X8-H Plus
 # Specs: Amlogic S812-H SoC, 2 GB RAM, 10/100 Mbps LAN, AP6335e WiFi
 PROJECT=Minix-S8X2 DEVICE=X8H-Plus ARCH=arm make image -j8
 
-# Universal build for n200-based S812 clones
+# Universal build for multiple n200-based S812 TV boxes
 # Specs: Amlogic S812 SoC, 2 GB RAM, 10/100 Mbps LAN, AP6330 WiFi
 PROJECT=Minix-S8X2 DEVICE=n200-AP6330_2G ARCH=arm make image -j8
 
-# WeTek Core
+# WeTek Core (Semi-official)
 # Specs: Amlogic S812-H SoC, 2 GB RAM, Gigabit LAN, AP6330 WiFi
 PROJECT=WeTek_Core ARCH=arm make image -j8
 
-# MXQ-S85
+# MXQ S85
 # Specs: Amlogic S805 SoC, 1 GB RAM, 10/100 LAN, AP6210/AP6212 WiFi
 PROJECT=S805 DEVICE=MXQ ARCH=arm make image -j8
 
