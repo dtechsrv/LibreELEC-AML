980bb2300cf0f02d2fe97f62b8dd12d4a17c5cbf
dtechsrv <dee.gabor@gmail.com>
2021-12-23 08:49:58 +0100

    projects/S8X2: add universal build for multiple k200-based S802 TV boxes

--- build_3rdParty.sh
+++ build_3rdParty.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # Create image for 3rdParty devices
 
+# Universal build for multiple k200-based S802 TV boxes
+# Specs: Amlogic S802 SoC, 2 GB RAM, 10/100 Mbps LAN, AP6330 or Realtek WiFi
+PROJECT=S8X2 DEVICE=k200-AP6330_2G ARCH=arm make image -j8
+
+# Universal build for multiple n200-based S812 TV boxes
+# Specs: Amlogic S812 SoC, 2 GB RAM, 10/100 Mbps LAN, AP6330 or Realtek WiFi
+PROJECT=S8X2 DEVICE=n200-AP6330_2G ARCH=arm make image -j8
+
 # MXIII
 # Specs: Amlogic S802 SoC, 1 GB RAM, 10/100 Mbps LAN, AP6330 WiFi
 PROJECT=S8X2 DEVICE=MXIII-1G ARCH=arm make image -j8
@@ -9,21 +17,17 @@ PROJECT=S8X2 DEVICE=MXIII-1G ARCH=arm make image -j8
 # Specs: Amlogic S812 SoC, 2 GB RAM, Gigabit LAN, AP6234 WiFi
 PROJECT=S8X2 DEVICE=MXIII-Plus ARCH=arm make image -j8
 
-# Universal build for multiple n200-based S812 TV boxes
-# Specs: Amlogic S812 SoC, 2 GB RAM, 10/100 Mbps LAN, AP6330 WiFi
-PROJECT=S8X2 DEVICE=n200-AP6330_2G ARCH=arm make image -j8
-
-# MINIX Neo X8-H Plus
-# Specs: Amlogic S812-H SoC, 2 GB RAM, 10/100 Mbps LAN, AP6335e WiFi
-PROJECT=Minix-S8X2 DEVICE=X8H-Plus ARCH=arm make image -j8
-
 # MINIX Neo X8 / X8-H
 # Specs: Amlogic S802(-H) SoC, 2 GB RAM, 10/100 Mbps LAN, AP6330 WiFi
 PROJECT=Minix-S8X2 DEVICE=X8-X8H ARCH=arm make image -j8
 
+# MINIX Neo X8-H Plus
+# Specs: Amlogic S812-H SoC, 2 GB RAM, Gigabit Mbps LAN, AP6335e WiFi
+PROJECT=Minix-S8X2 DEVICE=X8H-Plus ARCH=arm make image -j8
+
 # MXQ S85
 # Specs: Amlogic S805 SoC, 1 GB RAM, 10/100 LAN, AP6210/AP6212 WiFi
-PROJECT=S805 DEVICE=MXQ ARCH=arm make image -j8
+#PROJECT=S805 DEVICE=MXQ ARCH=arm make image -j8
 
 # Remove unnecessary files after the build process
 rm -rf target/*.kernel
