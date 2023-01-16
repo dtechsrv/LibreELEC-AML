5dd3d34a66612bf66b263a46a22d6589eb613ed6
dtechsrv <dee.gabor@gmail.com>
2023-01-16 14:31:07 +0100

    aml-vnc: fix package details and copyright headers

--- packages/3rdparty/service/aml-vnc/package.mk
+++ packages/3rdparty/service/aml-vnc/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+# Copyright (C) 2016-2020 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2021-present Gabor Dee (dee.gabor@gmail.com)
 
 PKG_NAME="aml-vnc"
 PKG_VERSION="f87a8f41aa6c6463a99ec3be9aebaff3cf31c5ee"
@@ -8,7 +9,8 @@ PKG_REV="103"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kszaq/aml-vnc"
-PKG_URL="https://github.com/kszaq/my-addons/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/kszaq/my-addons/archive/${PKG_VERSION}.tar.gz"
+PKG_MAINTAINER="dtech(.hu)"
 PKG_DEPENDS_TARGET="toolchain libvncserver"
 PKG_SECTION="service/system"
 PKG_SHORTDESC="Amlogic VNC server"
@@ -19,9 +21,9 @@ PKG_ADDON_NAME="Amlogic VNC"
 PKG_ADDON_TYPE="xbmc.service"
 
 unpack() {
-  mkdir -p $PKG_BUILD/addon
-  tar --strip-components=3 -xf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz -C $PKG_BUILD my-addons-$PKG_VERSION/$PKG_NAME/sources
-  tar --strip-components=3 -xf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz -C $PKG_BUILD/addon my-addons-$PKG_VERSION/$PKG_NAME/source
+  mkdir -p ${PKG_BUILD}/addon
+  tar --strip-components=3 -xf ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz -C ${PKG_BUILD} my-addons-${PKG_VERSION}/${PKG_NAME}/sources
+  tar --strip-components=3 -xf ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz -C ${PKG_BUILD}/addon my-addons-${PKG_VERSION}/${PKG_NAME}/source
 }
 
 makeinstall_target() {
@@ -29,7 +31,7 @@ makeinstall_target() {
 }
 
 addon() {
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp -P $PKG_BUILD/aml-vnc $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp -PR $PKG_BUILD/addon/* $ADDON_BUILD/$PKG_ADDON_ID
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+  cp -P ${PKG_BUILD}/aml-vnc ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+  cp -PR ${PKG_BUILD}/addon/* ${ADDON_BUILD}/${PKG_ADDON_ID}
 }
