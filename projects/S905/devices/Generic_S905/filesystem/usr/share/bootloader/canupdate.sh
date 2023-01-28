6acffb2ccc623a18add6e0bef8e7e495c44f7fcf
dtechsrv <dee.gabor@gmail.com>
2023-01-28 12:21:39 +0100

    canupdate.sh: add update support from MXQ-4K-Pro_1G to Generic_S905

--- projects/S905/devices/Generic_S905/filesystem/usr/share/bootloader/canupdate.sh
+++ projects/S905/devices/Generic_S905/filesystem/usr/share/bootloader/canupdate.sh
@@ -1,5 +1,5 @@
 # Allow upgrades from previous MXQ-4K-Pro_2G image
-if [ "$1" = "MXQ-4K-Pro_2G.arm" ]; then
+if [ "$1" = "MXQ-4K-Pro_2G.arm" ] || [ "$1" = "MXQ-4K-Pro_1G.arm" ]; then
   exit 0
 else
   exit 1
