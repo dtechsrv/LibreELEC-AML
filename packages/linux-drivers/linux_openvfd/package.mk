e385c6d2a513ed79a639f55da77483bae3e29c25
dtechsrv <dee.gabor@gmail.com>
2023-02-04 17:45:10 +0100

    linux-drivers: add linux_openvfd package

---
+++ packages/linux-drivers/linux_openvfd/package.mk
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018 Arthur Liberman (arthur_liberman (at) hotmail.com)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="linux_openvfd"
+PKG_VERSION="d753dd21ae9345b310111c5de43de3db4927c871"
+PKG_SHA256="ef3a0077aa5a83093d48980d998d6abf0f07cb16c95879ee7c5d80de0b40b588"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPL3"
+PKG_SITE="https://github.com/arthur-liberman/linux_openvfd"
+PKG_URL="https://github.com/arthur-liberman/linux_openvfd/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_LONGDESC="Linux OpenVFD is an open source Linux driver for VFD displays"
+PKG_TOOLCHAIN="manual"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make ARCH=$TARGET_KERNEL_ARCH \
+       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+       -C "$(kernel_path)" M="$PKG_BUILD/driver"
+  make OpenVFDService
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    find $PKG_BUILD/ -name \*.ko -not -path '*/\.*' -exec cp {} $INSTALL/$(get_full_module_dir)/$PKG_NAME \;
+
+  mkdir -p $INSTALL/usr/bin
+    cp -P $PKG_DIR/scripts/openvfd-config $INSTALL/usr/bin
+    cp -P $PKG_DIR/scripts/openvfd $INSTALL/usr/bin
+
+  mkdir -p $INSTALL/usr/sbin
+    cp -P OpenVFDService $INSTALL/usr/sbin
+}
+
+post_install() {
+  enable_service openvfd-config.service
+  enable_service openvfd.service
+}
