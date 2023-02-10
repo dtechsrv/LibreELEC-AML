5c33f1cc3ae204232f706b67e35e633e57862b86
dtechsrv <dee.gabor@gmail.com>
2023-02-10 19:52:41 +0100

    build.sh: use the real number of cpu threads for make

--- build.sh
+++ build.sh
@@ -2,31 +2,31 @@
 # Create image for supported devices
 
 # Mecool/Videostrong KI/KII-Pro/Plus series (gxbb, gxl)
-PROJECT=S905 DEVICE=K_Series ARCH=arm make image -j8
+PROJECT=S905 DEVICE=K_Series ARCH=arm make image -j$(nproc)
 
 # WeTek Play 2 (gxbb_p200_2G_wetek_play_2)
 # Specs: Amlogic S905-H SoC, 2 GB RAM, 8 GB eMMC, 1 Gbps Ethernet (RTL8211F), AP6330 WiFi.
-PROJECT=S905 DEVICE=WeTek_Play_2 ARCH=arm make amlpkg -j8
+PROJECT=S905 DEVICE=WeTek_Play_2 ARCH=arm make amlpkg -j$(nproc)
 
 # OTT M8S+ (m8m2_KII)
 # Specs: Amlogic S812 SoC, 2 GB RAM, 8 GB NAND Flash, 1 Gbps Ethernet (RTL8211F), AP6330 or BCM4335B0 WiFi.
-PROJECT=S8X2 DEVICE=M8S-Plus ARCH=arm make image -j8
+PROJECT=S8X2 DEVICE=M8S-Plus ARCH=arm make image -j$(nproc)
 
 # WeTek Core (m8m2_n200C_2G)
 # Specs: Amlogic S812-H SoC, 2 GB RAM, 8 GB eMMC, 1 Gbps Ethernet, AP6330 WiFi.
-PROJECT=S8X2 DEVICE=WeTek_Core ARCH=arm make amlpkg -j8
+PROJECT=S8X2 DEVICE=WeTek_Core ARCH=arm make amlpkg -j$(nproc)
 
 # MXQ HD18Q "all black" boxes (m8b_m201_v1)
 # Specs: Amlogic S805 SoC, 1 GB RAM, 8 GB NAND Flash, 10/100 Mbps Ethernet (IC+ IP101GR), Realtek WiFi.
-PROJECT=S805 DEVICE=HD18Q ARCH=arm make image -j8
+PROJECT=S805 DEVICE=HD18Q ARCH=arm make image -j$(nproc)
 
 # MXQ m201d with 512 MB RAM only (m8b_m201C_512M)
 # Specs: Amlogic S805 SoC, 1 GB RAM, 8 GB NAND Flash, 10/100 Mbps Ethernet (IC+ IP101GR), Realtek WiFi.
-PROJECT=S805 DEVICE=m201d ARCH=arm make image -j8
+PROJECT=S805 DEVICE=m201d ARCH=arm make image -j$(nproc)
 
 # WeTek Play (AML8726-MX, meson6 without dt-id)
 # Specs: Amlogic M6 SoC, 1 GB RAM, 4 GB NAND Flash, 10/100 Mbps Ethernet (SMSC LAN8720A), AP6210 WiFi.
-PROJECT=WeTek_Play ARCH=arm make amlpkg -j8
+PROJECT=WeTek_Play ARCH=arm make amlpkg -j$(nproc)
 
 # Remove unnecessary files after the build process
 rm -rf target/*.kernel
--- build_3rdParty.sh
+++ build_3rdParty.sh
@@ -2,46 +2,46 @@
 # Create image for 3rdParty devices
 
 # Generic S905x devices (gxbb, gxl)
-PROJECT=S905 DEVICE=Generic_S905 ARCH=arm make image -j8
+PROJECT=S905 DEVICE=Generic_S905 ARCH=arm make image -j$(nproc)
 
 # Generic S912 devices (gxm)
-PROJECT=S912 DEVICE=Generic_S912 ARCH=arm make image -j8
+PROJECT=S912 DEVICE=Generic_S912 ARCH=arm make image -j$(nproc)
 
 # Universal build for multiple k200-based S802 TV boxes
 # Specs: Amlogic S802 SoC, 2 GB RAM, 10/100 Mbps LAN, AP6330 or Realtek WiFi
-PROJECT=S8X2 DEVICE=k200-AP6330_2G ARCH=arm make image -j8
+PROJECT=S8X2 DEVICE=k200-AP6330_2G ARCH=arm make image -j$(nproc)
 
 # Universal build for multiple n200-based S812 TV boxes
 # Specs: Amlogic S812 SoC, 2 GB RAM, 10/100 Mbps LAN, AP6330 or Realtek WiFi
-PROJECT=S8X2 DEVICE=n200-AP6330_2G ARCH=arm make image -j8
+PROJECT=S8X2 DEVICE=n200-AP6330_2G ARCH=arm make image -j$(nproc)
 
 # MXIII
 # Specs: Amlogic S802 SoC, 1 GB RAM, 10/100 Mbps LAN, AP6330 WiFi
-PROJECT=S8X2 DEVICE=MXIII-1G ARCH=arm make image -j8
+PROJECT=S8X2 DEVICE=MXIII-1G ARCH=arm make image -j$(nproc)
 
 # MXIII Plus / G
 # Specs: Amlogic S812 SoC, 2 GB RAM, Gigabit LAN, AP6234 WiFi
-PROJECT=S8X2 DEVICE=MXIII-Plus ARCH=arm make image -j8
+PROJECT=S8X2 DEVICE=MXIII-Plus ARCH=arm make image -j$(nproc)
 
 # MINIX Neo X8 / X8-H
 # Specs: Amlogic S802(-H) SoC, 2 GB RAM, 10/100 Mbps LAN, AP6330 WiFi
-PROJECT=Minix-S8X2 DEVICE=X8-X8H ARCH=arm make image -j8
+PROJECT=Minix-S8X2 DEVICE=X8-X8H ARCH=arm make image -j$(nproc)
 
 # MINIX Neo X8-H Plus
 # Specs: Amlogic S812-H SoC, 2 GB RAM, Gigabit LAN, AP6335e WiFi
-PROJECT=Minix-S8X2 DEVICE=X8H-Plus ARCH=arm make image -j8
+PROJECT=Minix-S8X2 DEVICE=X8H-Plus ARCH=arm make image -j$(nproc)
 
 # Odroid C1/C1+
 # Specs: Amlogic S805 SoC, 1 GB RAM, Gigabit LAN, without WiFi
-PROJECT=Odroid_C1 ARCH=arm make image -j8
+PROJECT=Odroid_C1 ARCH=arm make image -j$(nproc)
 
 # WeTek Hub
 # Specs: Amlogic S905-H SoC, 1 GB RAM, Gigabit LAN, AP6335 or AP6255 WiFi
-PROJECT=S905 DEVICE=WeTek_Hub ARCH=arm make amlpkg -j8
+PROJECT=S905 DEVICE=WeTek_Hub ARCH=arm make amlpkg -j$(nproc)
 
 # MXQ S85
 # Specs: Amlogic S805 SoC, 1 GB RAM, 10/100 LAN, AP6210/AP6212 WiFi
-#PROJECT=S805 DEVICE=MXQ ARCH=arm make image -j8
+#PROJECT=S805 DEVICE=MXQ ARCH=arm make image -j$(nproc)
 
 # Remove unnecessary files after the build process
 rm -rf target/*.kernel
