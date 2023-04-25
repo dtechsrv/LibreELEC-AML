c53b089aacfede25be1a3eea5700bda91eb396e4
dtechsrv <dee.gabor@gmail.com>
2023-04-25 13:40:53 +0200

    aml-vnc: update addon (105)

--- packages/3rdparty/service/aml-vnc/package.mk
+++ packages/3rdparty/service/aml-vnc/package.mk
@@ -5,7 +5,7 @@
 PKG_NAME="aml-vnc"
 PKG_VERSION="f87a8f41aa6c6463a99ec3be9aebaff3cf31c5ee"
 PKG_SHA256="dbec8fbedc956b8730414654da4f90b035c1cb67e2320e5c572b1343fb029e36"
-PKG_REV="104"
+PKG_REV="105"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kszaq/aml-vnc"
@@ -13,21 +13,16 @@ PKG_URL="https://github.com/kszaq/my-addons/archive/${PKG_VERSION}.tar.gz"
 PKG_MAINTAINER="dtech(.hu)"
 PKG_DEPENDS_TARGET="toolchain libvncserver"
 PKG_SECTION="service/system"
-PKG_SHORTDESC="Amlogic VNC server"
-PKG_LONGDESC="Amlogic VNC server is a Virtual Network Computing (VNC) server for Amlogic devices"
+PKG_SHORTDESC="Amlogic VNC Server"
+PKG_LONGDESC="Amlogic VNC Server is a Virtual Network Computing (VNC) server for Amlogic devices"
 
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="Amlogic VNC"
+PKG_ADDON_NAME="Amlogic VNC Server"
 PKG_ADDON_TYPE="xbmc.service"
 
 unpack() {
-  # Copy aml-vnc sources
   mkdir -p ${PKG_BUILD}
   tar --strip-components=3 -xf ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz -C ${PKG_BUILD} my-addons-${PKG_VERSION}/${PKG_NAME}/sources
-
-  # Copy addon files
-  mkdir -p ${PKG_BUILD}/addon
-  tar --strip-components=3 -xf ${SOURCES}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz -C ${PKG_BUILD}/addon my-addons-${PKG_VERSION}/${PKG_NAME}/source
 }
 
 makeinstall_target() {
@@ -35,12 +30,7 @@ makeinstall_target() {
 }
 
 addon() {
-  # Install aml-vnc binary
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/{bin,lib}
   cp -P ${PKG_BUILD}/aml-vnc ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
-  cp -PR ${PKG_BUILD}/addon/* ${ADDON_BUILD}/${PKG_ADDON_ID}
-
-  # Install libvncserver library
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
   cp -PL $(get_build_dir libvncserver)/.install_pkg/usr/lib/libvncserver.so.? ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
 }
---
+++ packages/3rdparty/service/aml-vnc/source/default.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-2020 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2021-present Gabor Dee (dee.gabor@gmail.com)
