e57e3dfa4188b70ee982e3296d752fcc3942414e
dtechsrv <dee.gabor@gmail.com>
2023-04-25 17:10:45 +0200

    inadyn: update libconfuse to 3.3, inadyn to 2.10 and addon (106)

--- packages/addons/service/inadyn/package.mk
+++ packages/addons/service/inadyn/package.mk
@@ -2,17 +2,17 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inadyn"
-PKG_VERSION="2.5"
-PKG_SHA256="28fddd94cb7dda08aef0e5e97bbfd2af83f5dc7ac899a477b5936e82a76d3709"
-PKG_REV="105"
+PKG_VERSION="2.10.0"
+PKG_SHA256="a9bc24ea8d59644f909dd98bf3b8efcfb95c153f46d9cb62e99d744d44ed9465"
+PKG_REV="106"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="http://troglobit.com/inadyn.html"
-PKG_URL="https://github.com/troglobit/inadyn/archive/v$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/troglobit/inadyn/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libconfuse openssl"
 PKG_SECTION="service/system"
 PKG_SHORTDESC="Inadyn: a small and simple Dynamic Domain Name System client"
-PKG_LONGDESC="Inadyn ($PKG_VERSION) is a small and simple Dynamic Domain Name System (DDNS) client with HTTPS support. It is commonly available in many GNU/Linux distributions, used in off-the-shelf routers and Internet gateways to automate the task of keeping your DNS record up to date with any IP address changes from your ISP. It can also be used in installations with redundant (backup) connections to the Internet."
+PKG_LONGDESC="Inadyn (${PKG_VERSION}) is a small and simple Dynamic Domain Name System (DDNS) client with HTTPS support. It is commonly available in many GNU/Linux distributions, used in off-the-shelf routers and Internet gateways to automate the task of keeping your DNS record up to date with any IP address changes from your ISP. It can also be used in installations with redundant (backup) connections to the Internet."
 PKG_TOOLCHAIN="autotools"
 
 PKG_IS_ADDON="yes"
