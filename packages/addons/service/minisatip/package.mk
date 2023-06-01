1506022a308bfdac4a007cb150d8c2aeba965485
dtechsrv <dee.gabor@gmail.com>
2023-06-01 17:57:12 +0200

    minisatip: update to 1.2.83 and addon (102)

--- packages/addons/service/minisatip/package.mk
+++ packages/addons/service/minisatip/package.mk
@@ -2,18 +2,18 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="minisatip"
-PKG_VERSION="a3af6dd93c4fd9dbbb62073f3642a289826d84bb"
-PKG_SHA256="32d17edab044f9fdba9e39bb4a8332aa087951695c1629d81687ff957c8d601d"
-PKG_VERSION_NUMBER="0.7.18"
-PKG_REV="101"
+PKG_VERSION="1.2.83"
+PKG_SHA256="303d1e4e56d80ecbbfd5f08c4857dce9dc7133b7f8cfbdfc40a10fd4edc55879"
+PKG_REV="102"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/catalinii/minisatip"
-PKG_URL="https://github.com/catalinii/minisatip/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain dvb-apps libdvbcsa libxml2 openssl"
+PKG_URL="https://github.com/catalinii/minisatip/archive/refs/tags/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libdvbcsa libxml2 openssl"
 PKG_SECTION="service"
 PKG_SHORTDESC="minisatip: a Sat>IP streaming server for Linux"
-PKG_LONGDESC="minisatip($PKG_VERSION_NUMBER): is a Sat>IP streaming server for Linux supporting DVB-C, DVB-S/S2, DVB-T/T2, ATSC and ISDB-T"
+PKG_LONGDESC="minisatip(${PKG_VERSION_NUMBER}): is a Sat>IP streaming server for Linux supporting DVB-C, DVB-S/S2, DVB-T/T2, ATSC and ISDB-T"
+PKG_BUILD_FLAGS="-sysroot"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Minisatip"
@@ -27,12 +27,8 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --with-xml2=$(get_build_dir libxml2)"
 
 pre_configure_target() {
-  cd $PKG_BUILD
-    rm -rf .$TARGET_NAME
-
-  # enables Common Interface (DVBEN50221)
-  CFLAGS+=" -I$(get_build_dir dvb-apps)/lib"
-  LDFLAGS+=" -L$(get_build_dir dvb-apps)/lib/libdvbapi -L$(get_build_dir dvb-apps)/lib/libdvben50221 -L$(get_build_dir dvb-apps)/lib/libucsi"
+  cd ${PKG_BUILD}
+    rm -rf .${TARGET_NAME}
 }
 
 makeinstall_target() {
@@ -40,8 +36,8 @@ makeinstall_target() {
 }
 
 addon() {
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $PKG_BUILD/minisatip $ADDON_BUILD/$PKG_ADDON_ID/bin
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/webif
-    cp -PR $PKG_BUILD/html/* $ADDON_BUILD/$PKG_ADDON_ID/webif
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    cp -P ${PKG_BUILD}/minisatip ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/webif
+    cp -PR ${PKG_BUILD}/html/* ${ADDON_BUILD}/${PKG_ADDON_ID}/webif
 }
