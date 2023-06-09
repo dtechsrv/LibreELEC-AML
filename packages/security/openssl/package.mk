059ba1e124c431471669f6a9965993f8e0c02f3b
dtechsrv <dee.gabor@gmail.com>
2023-06-09 12:03:41 +0200

    openssl: update to 1.1.1u, cacert.pem: update to 2023-05-30

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openssl"
-PKG_VERSION="1.1.1t"
-PKG_SHA256="8dee9b24bdb1dcbf0c3d1e9b02fb8f6bf22165e807f45adeb7c9677536859d3b"
+PKG_VERSION="1.1.1u"
+PKG_SHA256="e2f8d84b523eecd06c7be7626830370300fbcc15386bf5142d72758f6963ebc6"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.openssl.org"
 PKG_URL="https://www.openssl.org/source/${PKG_NAME}-${PKG_VERSION}.tar.gz"
