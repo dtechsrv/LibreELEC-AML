73e92584697a0fd103972e2e4116b9567677e36a
dtechsrv <dee.gabor@gmail.com>
2023-09-18 12:27:08 +0200

    update.sh: add missing script to WP1 and missing section to WP2/Hub

--- projects/S905/devices/WeTek_Hub/filesystem/usr/share/bootloader/update.sh
+++ projects/S905/devices/WeTek_Hub/filesystem/usr/share/bootloader/update.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2016-2023 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2023-present Gabor Dee (dee.gabor@gmail.com)
 
 [ -z "$SYSTEM_ROOT" ] && SYSTEM_ROOT=""
 [ -z "$BOOT_ROOT" ] && BOOT_ROOT="/flash"
 
 IMAGE_KERNEL="/flash/kernel.img"
 
-# parse command line arguments
 for arg in $(cat /proc/cmdline); do
   case $arg in
     BOOT_IMAGE=*)
@@ -17,9 +17,7 @@ for arg in $(cat /proc/cmdline); do
   esac
 done
 
-
-# mount $BOOT_ROOT r/w
-  mount -o remount,rw $BOOT_ROOT
+mount -o remount,rw $BOOT_ROOT
 
 if [ -b $IMAGE_KERNEL ]; then
   dd if="$SYSTEM_ROOT/usr/share/bootloader/dtb.img" of="/dev/dtb" bs=262144 2>&1
@@ -27,6 +25,15 @@ else
   cp -p $SYSTEM_ROOT/usr/share/bootloader/dtb.img $BOOT_ROOT
 fi
 
-# mount $BOOT_ROOT r/o
-  sync
-  mount -o remount,ro $BOOT_ROOT
+sync
+mount -o remount,ro $BOOT_ROOT
+
+if [ -e /storage/.kodi/addons/repository.libreelec.tv ]; then
+  echo "Remove outdated LibreELEC Add-ons update..."
+  rm -rf /storage/.kodi/addons/repository.libreelec.tv
+fi
+
+if [ -e /storage/.kodi/addons/service.libreelec.settings ]; then
+  echo "Remove outdated LibreELEC Configuration update..."
+  rm -rf /storage/.kodi/addons/service.libreelec.settings
+fi
---
+++ projects/WeTek_Play/filesystem/usr/share/bootloader/update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2017-2023 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2023-present Gabor Dee (dee.gabor@gmail.com)
+
+if [ -e /storage/.kodi/addons/repository.libreelec.tv ]; then
+  echo "Remove outdated LibreELEC Add-ons update..."
+  rm -rf /storage/.kodi/addons/repository.libreelec.tv
+fi
+
+if [ -e /storage/.kodi/addons/service.libreelec.settings ]; then
+  echo "Remove outdated LibreELEC Configuration update..."
+  rm -rf /storage/.kodi/addons/service.libreelec.settings
+fi
