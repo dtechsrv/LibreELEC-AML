95d9661618e96c9d9c6a4d7d7eb089c725d30615
dtechsrv <dee.gabor@gmail.com>
2023-10-19 09:16:38 +0200

    S905: merge K_Series into Generic_S905

--- projects/S905/devices/Generic_S905/bootloader/canupdate.sh
+++ projects/S905/devices/Generic_S905/bootloader/canupdate.sh
@@ -1,5 +1,8 @@
-# Allow upgrades from previous MXQ-4K-Pro_2G image
+# Allow upgrades from previous MXQ-4K-Pro_2G and *1G images
 if [ "$1" = "MXQ-4K-Pro_2G.arm" ] || [ "$1" = "MXQ-4K-Pro_1G.arm" ]; then
+  exit 0
+# Allow upgrades from previous KI-Pro, KI-Plus and K_Series images
+elif [ "$1" = "KI-Pro.arm" ] || [ "$1" = "KI-Plus.arm" ] || [ "$1" = "K_Series.arm" ]; then
   exit 0
 else
   exit 1
