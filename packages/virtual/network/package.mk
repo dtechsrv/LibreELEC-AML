2a337c9bb3b4a6cb5dedaa32e53d390c47ff3373
dtech(.hu) <dee.gabor@gmail.com>
2024-01-25 16:14:16 +0100

    inputstream.adaptive: fix missing dependency for aarch64

--- packages/virtual/network/package.mk
+++ packages/virtual/network/package.mk
@@ -10,23 +10,23 @@ PKG_DEPENDS_TARGET="toolchain connman netbase ethtool openssh iw"
 PKG_SECTION="virtual"
 PKG_LONGDESC="Metapackage for various packages to install network support"
 
-if [ "$BLUETOOTH_SUPPORT" = "yes" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bluez"
+if [ "${BLUETOOTH_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" bluez"
 fi
 
-if [ "$SAMBA_SERVER" = "yes" ] || [ "$SAMBA_SUPPORT" = "yes" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET samba"
+if [ "${SAMBA_SERVER}" = "yes" ] || [ "${SAMBA_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" samba"
 fi
 
-if [ "$OPENVPN_SUPPORT" = "yes" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET openvpn"
+if [ "${OPENVPN_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" openvpn"
 fi
 
-if [ "$WIREGUARD_SUPPORT" = "yes" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET wireguard-tools wireguard-linux-compat"
+if [ "${WIREGUARD_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" wireguard-tools wireguard-linux-compat"
 fi
 
 # nss needed by inputstream.adaptive, chromium etc.
-if [ "$TARGET_ARCH" = "x86_64" ] || [ "$TARGET_ARCH" = "arm" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET nss"
+if [ "${TARGET_ARCH}" = "x86_64" ] || [ "${TARGET_ARCH}" = "arm" ] || [ "${TARGET_ARCH}" = "aarch64" ]; then
+  PKG_DEPENDS_TARGET+=" nss"
 fi
