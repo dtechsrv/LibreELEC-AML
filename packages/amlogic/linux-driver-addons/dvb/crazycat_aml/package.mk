8d87afed6b4cbc299d39d77b6e8771de1530865a
Gábor Deé <dee.gabor@gmail.com>
2024-12-12 11:39:53 +0100

    crazycat_aml: update to 2019-07-11

--- packages/amlogic/linux-driver-addons/dvb/crazycat_aml/package.mk
+++ packages/amlogic/linux-driver-addons/dvb/crazycat_aml/package.mk
@@ -4,12 +4,12 @@
 # Copyright (C) 2024-present Gabor Dee (dee.gabor@gmail.com)
 
 PKG_NAME="crazycat_aml"
-PKG_VERSION="835dc72da3ee63df7f4057bd0507887454c005d1"
-PKG_SHA256="3d68d368a9eda15688c6686caa854a045a753740ec93553d80a4bcfc14c2950a"
+PKG_VERSION="2019-07-11"
+PKG_SHA256="4f8cca130d0d941f930fb6e537905db40e5fc770f4e3c15b8e36a0ffb98b9194"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://bitbucket.org/CrazyCat/media_build"
-PKG_URL="https://bitbucket.org/CrazyCat/media_build/get/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/dtechsrv/media_build/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux media_tree_cc_aml"
 PKG_NEED_UNPACK="${LINUX_DEPENDS} $(get_pkg_directory media_tree_cc_aml)"
 PKG_SECTION="driver.dvb"
@@ -24,10 +24,10 @@ PKG_ADDON_VERSION="${ADDON_VERSION}.${PKG_REV}"
 
 case "${LINUX}" in
   amlogic-3.10)
-    PKG_PATCH_DIRS+="amlogic-3.10"
+    PKG_PATCH_DIRS+=" amlogic-3.10"
     ;;
   amlogic-3.14)
-    PKG_PATCH_DIRS+="amlogic-3.14"
+    PKG_PATCH_DIRS+=" amlogic-3.14"
     PKG_DEPENDS_TARGET="${PKG_DEPENDS_TARGET} media_tree_aml"
     PKG_NEED_UNPACK="${PKG_NEED_UNPACK} $(get_pkg_directory media_tree_aml)"
     ;;
@@ -77,10 +77,10 @@ make_target() {
 
   # hack to workaround media_build bug
   sed -e 's/CONFIG_VIDEO_S5C73M3=m/# CONFIG_VIDEO_S5C73M3 is not set/g' -i ${PKG_BUILD}/v4l/.config
-  sed -e 's/CONFIG_VIDEO_SAA7146_VV=m/# CONFIG_VIDEO_SAA7146_VV is not set/g' -i ${PKG_BUILD}/v4l/.config
-  sed -e 's/CONFIG_VIDEO_S5K5BAF=m/# CONFIG_VIDEO_S5K5BAF is not set/g' -i ${PKG_BUILD}/v4l/.config
-  sed -e 's/CONFIG_VIDEO_TVP514X=m/# CONFIG_VIDEO_TVP514X is not set/g' -i ${PKG_BUILD}/v4l/.config
-  sed -e 's/CONFIG_VIDEO_TVP7002=m/# CONFIG_VIDEO_TVP7002 is not set/g' -i ${PKG_BUILD}/v4l/.config
+
+  if [ ${LINUX} = "amlogic-3.10" ]; then
+    sed -e 's/CONFIG_V4L_AMLOGIC_VIDEO2=m/# CONFIG_V4L_AMLOGIC_VIDEO2 is not set/g' -i ${PKG_BUILD}/v4l/.config
+  fi
 
   kernel_make VER=${KERNEL_VER} SRCDIR=$(kernel_path)
 }
