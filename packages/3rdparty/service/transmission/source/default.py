94b8f88a67ace939dbd07dce782f194db9664a33
Gábor Deé <dee.gabor@gmail.com>
2024-12-25 18:16:35 +0100

    transmission: remove downloadmanager subcategory and reset addon (100)
    - reset addon version
    - remove downloadmanager subcategory
    - rewrite service start script
    - rewrite configuration settings
    - rework localization files
    - add restart option when config changes

--- packages/3rdparty/service/transmission/source/default.py
+++ packages/3rdparty/service/transmission/source/default.py
@@ -1,3 +1,19 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+# Copyright (C) 2018-2021 Frank Hartung (supervisedthinking (@) gmail.com)
+# Copyright (C) 2022-present Gabor Dee (dee.gabor@gmail.com)
+
+import subprocess
+import xbmc
+import xbmcaddon
+
+class Monitor(xbmc.Monitor):
+   def __init__(self, *args, **kwargs):
+      xbmc.Monitor.__init__(self)
+      self.id = xbmcaddon.Addon().getAddonInfo('id')
+
+   def onSettingsChanged(self):
+      subprocess.call(['systemctl', 'restart', self.id])
+
+if __name__ == "__main__":
+   Monitor().waitForAbort()
