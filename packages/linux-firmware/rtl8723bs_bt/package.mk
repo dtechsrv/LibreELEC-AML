ab0a76e5fc5fc98a21d6d2095c67e83f2e41a26d
dtech <dee.gabor@gmail.com>
2024-12-29 16:41:10 +0100

    linux: add Realtek BT stack for amlogic 3.10 and 3.14 (#42)
    * linux: remove a patch that causes a potential conflict
    * linux: add realtek bluetooth support
    * rtl8723bs_bt: move firmware files from projects to package dir
    * kernel-firmware: add firmware files for RTL8761B

--- packages/linux-firmware/rtl8723bs_bt/package.mk
+++ packages/linux-firmware/rtl8723bs_bt/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2017-2023 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2024-present Gabor Dee (dee.gabor@gmail.com)
 
 PKG_NAME="rtl8723bs_bt"
 PKG_VERSION="09eb91f"
@@ -9,3 +10,11 @@ PKG_SITE="https://github.com/lwfinger/rtl8723bs_bt"
 PKG_URL="https://github.com/lwfinger/rtl8723bs_bt/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="rtl8723bs_bt Linux firmware"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/$(get_full_firmware_dir)/rtl_bt
+  cp -PR ${PKG_DIR}/firmware/rtl_bt/*.bin ${INSTALL}/$(get_full_firmware_dir)/rtl_bt
+
+  mkdir -p ${INSTALL}/usr/bin
+  cp -P ${PKG_BUILD}/rtk_hciattach ${INSTALL}/usr/bin
+}
