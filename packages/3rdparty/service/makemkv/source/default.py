2641303d0d2a5b7a11db8727829cab21ccced944
Gábor Deé <dee.gabor@gmail.com>
2025-01-29 18:57:19 +0100

    makemkv: tweak scripts and create addon settings

--- packages/3rdparty/service/makemkv/source/default.py
+++ packages/3rdparty/service/makemkv/source/default.py
@@ -1,2 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2025-present Gabor Dee (dee.gabor@gmail.com)
+
+import subprocess
+import xbmc
+import xbmcaddon
+
+class Monitor(xbmc.Monitor):
+   def __init__(self, *args, **kwargs):
+      xbmc.Monitor.__init__(self)
+      self.id = xbmcaddon.Addon().getAddonInfo('id')
+
+   def onSettingsChanged(self):
+      subprocess.call(['systemctl', 'restart', self.id])
+
+if __name__ == "__main__":
+   Monitor().waitForAbort()
