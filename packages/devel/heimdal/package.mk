7ee42e32e60dc4086e830632c3ecfff63f0e156c
Gábor Deé <dee.gabor@gmail.com>
2025-02-04 13:05:24 +0100

    samba: fix heimdal build issues

--- packages/devel/heimdal/package.mk
+++ packages/devel/heimdal/package.mk
@@ -13,7 +13,8 @@ PKG_LONGDESC="Kerberos 5, PKIX, CMS, GSS-API, SPNEGO, NTLM, Digest-MD5 and, SASL
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="-parallel"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
+PKG_CONFIGURE_OPTS_HOST="ac_cv_prog_COMPILE_ET=no \
+                         --enable-static --disable-shared \
                          --without-openldap \
                          --without-capng \
                          --without-sqlite3 \
@@ -29,5 +30,6 @@ PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
 
 makeinstall_host() {
   mkdir -p $TOOLCHAIN/bin
-    cp -PR lib/asn1/asn1_compile $TOOLCHAIN/bin
+    cp -PR lib/asn1/asn1_compile $TOOLCHAIN/bin/heimdal_asn1_compile
+    cp -PR lib/com_err/compile_et $TOOLCHAIN/bin/heimdal_compile_et
 }
