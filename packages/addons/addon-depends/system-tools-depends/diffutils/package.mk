c6a5313313f9dc16aab89058aebd950c241850c4
Gábor Deé <dee.gabor@gmail.com>
2025-02-05 09:35:47 +0100

    diffutils: update to 3.8

--- packages/addons/addon-depends/system-tools-depends/diffutils/package.mk
+++ packages/addons/addon-depends/system-tools-depends/diffutils/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="diffutils"
-PKG_VERSION="3.6"
-PKG_SHA256="d621e8bdd4b573918c8145f7ae61817d1be9deb4c8d2328a65cea8e11d783bd6"
+PKG_VERSION="3.8"
+PKG_SHA256="a6bdd7d1b31266d11c4f4de6c1b748d4607ab0231af5188fc2533d0ae2438fec"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/diffutils/"
 PKG_URL="http://ftpmirror.gnu.org/diffutils/$PKG_NAME-$PKG_VERSION.tar.xz"
@@ -15,7 +15,3 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-nls \
         --without-libsigsegv-prefix \
         --without-libiconv-prefix \
         --without-libintl-prefix"
-
-makeinstall_target() {
-  : # nop
-}
