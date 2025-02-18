d77e6e2c2dd25ec7cb34697732d12710bdb96a2f
Gábor Deé <dee.gabor@gmail.com>
2025-02-18 17:09:52 +0100

    opengl-meson-t82x: update to 3.2-r16p0-00rel0

--- packages/amlogic/graphics/opengl-meson-t82x/package.mk
+++ packages/amlogic/graphics/opengl-meson-t82x/package.mk
@@ -2,19 +2,18 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="opengl-meson-t82x"
-PKG_VERSION="915cb48"
-PKG_SHA256="9b5f65afa21250b67578c250da030a5829e69131ce91b2f167b01b1ed30be781"
+PKG_VERSION="3.2-r16p0-00rel0"
+PKG_SHA256="814512d1256c8a4784a033e8d58abbfee8e379b8badc8ed24f1266ad501697ab"
 PKG_LICENSE="nonfree"
-PKG_SITE="https://github.com/kszaq/opengl-meson-t82x"
-PKG_URL="https://github.com/kszaq/opengl-meson-t82x/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/dtechsrv/opengl-meson-t82x"
+PKG_URL="https://github.com/dtechsrv/opengl-meson-t82x/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libhybris"
-PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION*"
 PKG_LONGDESC="OpenGL ES pre-compiled libraries for Mali GPUs. The libraries were extracted from Khadas VIM2 Android firmware."
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/system
-    cp -a system/* $INSTALL/system
+  mkdir -p ${INSTALL}/system
+    cp -a system/* ${INSTALL}/system
 }
 
 post_install() {
