5556f49fc1298daa95334ff7cde7a5117a5010f7
Gábor Deé <dee.gabor@gmail.com>
2025-02-19 15:41:56 +0100

    avl6862-aml: use the original source files

--- packages/amlogic/linux-drivers/avl6862-aml/package.mk
+++ packages/amlogic/linux-drivers/avl6862-aml/package.mk
@@ -1,14 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2017-2021 Team LibreELEC (https://libreelec.tv)
 # Copyright (C) 2021-present Gabor Dee (dee.gabor@gmail.com)
 
 PKG_NAME="avl6862-aml"
-PKG_VERSION="7f02154e0835784fc07dc19312ed69b0b1a260f2"
-PKG_SHA256="9fc7f1fa1c53825926a2bb676d9c290a4ff986c4cb80f66136bc9474ad11a0f4"
+PKG_VERSION="690f930d580d09a5c6d8e5288a69f520e6a2ac9d"
+PKG_SHA256="78dae7eb253f36978f9e2d3be37923e6bd64ddddfebcfb63c6b997e51b6e36e3"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/dtechsrv/avl6862-aml"
-PKG_URL="https://github.com/dtechsrv/avl6862-aml/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/dtechsrv/dvb_tv-aml"
+PKG_URL="https://github.com/dtechsrv/dvb_tv-aml/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="${LINUX_DEPENDS}"
 PKG_LONGDESC="Availink AVL6862 internal DVB tuner driver for Amlogic devices developed by afl1"
